bacb011075a7c98a03a0feb4d515ca50fbb51dba
Brian Diego de Souza <Brian-desouza@hotmail.com>
2019-05-08 23:16:55 -0300

    incluindo bindable property

--- Templates/Templates/CustomControls/GradientFrame.cs
+++ Templates/Templates/CustomControls/GradientFrame.cs
@@ -5,7 +5,21 @@ namespace Templates.CustomControls
 {
     public class GradientFrame : Frame
     {
-        public Color StartColor { get; set; }
-        public Color EndColor { get; set; }
+        public static readonly BindableProperty StartColorProperty =
+            BindableProperty.Create("StartColor", typeof(Xamarin.Forms.Color), typeof(GradientFrame));
+
+        public static readonly BindableProperty EndColorProperty =
+            BindableProperty.Create("EndColor", typeof(Xamarin.Forms.Color), typeof(GradientFrame));
+        public Color StartColor
+        {
+            get { return (Color)GetValue(StartColorProperty); }
+            set { SetValue(StartColorProperty, value); }
+        }
+
+        public Color EndColor
+        {
+            get { return (Color)GetValue(EndColorProperty); }
+            set { SetValue(EndColorProperty, value); }
+        }
     }
 }
